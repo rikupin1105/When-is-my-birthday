3356e4fd08491538652e243c8dcf221063b37110
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-11-28 15:21:03 +0900

    init

---
+++ DialogFlowRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace When_is_my_birthday
+{
+    public class DialogFlowRequest
+    {
+        public string responseId { get; set; }
+        public Queryresult queryResult { get; set; }
+        public Originaldetectintentrequest originalDetectIntentRequest { get; set; }
+        public string session { get; set; }
+    }
+
+    public class Queryresult
+    {
+        public string queryText { get; set; }
+        public Parameters parameters { get; set; }
+        public bool allRequiredParamsPresent { get; set; }
+        public Fulfillmentmessage[] fulfillmentMessages { get; set; }
+        public Intent intent { get; set; }
+        public float intentDetectionConfidence { get; set; }
+        public string languageCode { get; set; }
+    }
+
+    public class Parameters
+    {
+        public float number { get; set; }
+    }
+
+    public class Intent
+    {
+        public string name { get; set; }
+        public string displayName { get; set; }
+        public bool endInteraction { get; set; }
+    }
+
+    public class Fulfillmentmessage
+    {
+        public Text text { get; set; }
+    }
+
+    public class Text
+    {
+        public string[] text { get; set; }
+    }
+
+    public class Originaldetectintentrequest
+    {
+        public Payload payload { get; set; }
+    }
+
+    public class Payload
+    {
+    }
+
+}
---
+++ Function1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace When_is_my_birthday
+{
+    public static class Function1
+    {
+        [FunctionName("When-is-my-bitrhday")]
+        public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req, ILogger log)
+        {
+            var data = req.Content.ReadAsAsync<DialogFlowRequest>().Result;
+            var age = data.queryResult.parameters.number;
+
+            var Datetime = DateTime.Now;
+            var Year = Datetime.Year;
+            var borned_year1 = Year - age;
+            var borned_year2 = borned_year1--;
+
+            var ResponceObject = new DialogFlowResponce();
+
+            var wareki_1 = Wareki(age,borned_year1);
+            var wareki_2 = Wareki(age,borned_year2);
+
+            ResponceObject.fulfillmentText = $"{borned_year1}年({wareki_1})、もしくは{borned_year2}年({wareki_2})です。";
+            string json = JsonConvert.SerializeObject(ResponceObject);
+
+            var ReturnObject = new ObjectResult(json);
+            return ReturnObject;
+        }
+        public static string Wareki(float age, float year)
+        {
+            var culture = new CultureInfo("ja-JP", true);
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            string strTime_1 = year + "/01/01";
+            string strTime_2 = year + "/12/13";
+
+            var data_1 = DateTime.Parse(strTime_1);
+            var data_2 = DateTime.Parse(strTime_2);
+
+            var borned_year_1 = data_1.ToString("ggyy年", culture);
+            var borned_year_2 = data_2.ToString("ggyy年", culture);
+
+            if (borned_year_1 == borned_year_2)
+            {
+                return borned_year_1;
+            }
+            else
+            {
+                return borned_year_1 + "," + borned_year_2;
+            }
+        }
+        class DialogFlowResponce
+        {
+            public string fulfillmentText { get; set; }
+        }
+    }
+}
